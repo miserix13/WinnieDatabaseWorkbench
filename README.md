133be232369bfe56a68528bb26c45a30837ba30d
JoJo Webster <151088710+miserix13@users.noreply.github.com>
2025-12-06 15:47:05 -0600

    Add project files.

---
+++ README.md
@@ -0,0 +1,6 @@
+﻿Winnie Database Workbench is a Windows-Forms application for working with databases.
+
+- Features:
+	- Query Editor
+	- Visual Designer for data definition and schema
+	- Management / Admin tooling
---
+++ WorkbenchForm.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace WinnieDatabaseWorkbench
+{
+    public class WorkbenchForm : Form
+    {
+        private void InitializeComponent()
+        {
+            this.Text = "Database Workbench";
+        }
+
+        public WorkbenchForm() :
+            base()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
